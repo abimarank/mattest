05d4a607d6d5ccc528e1e6a34213f80d94c3e97d
krishanthy <kirijame@gmail.com>
2013-08-13 20:36:49 +0530

    created Mattest1

---
+++ src/mattest/Mattest1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mattest;
+
+/**
+ *
+ * @author Krishanthy
+ */
+public class Mattest1 {
+    int x;
+    int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    
+    
+}
